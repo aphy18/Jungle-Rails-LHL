3477a1eef3bd11582d94344685ca7f7654d09ad1
aphy18 <aphason1@.com>
2021-09-17 12:58:22 +0000

    initialized users requests

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
 
+  end
+
+
     # These routes will be for signup. The first renders a form in the browse, the second will 
     # receive the form and create a user in our database using the data given to us by the user.
     get '/signup' => 'users#new'
     post '/users' => 'users#create'
-
-  end
 
   
  

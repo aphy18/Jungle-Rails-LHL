b3466f1f8d99f22c9d797009079dda15e7a94582
aphy18 <aphason1@.com>
2021-09-17 21:36:18 +0000

    added login and signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,13 @@ Rails.application.routes.draw do
 
     # These routes will be for signup. The first renders a form in the browse, the second will 
     # receive the form and create a user in our database using the data given to us by the user.
+
     get '/signup' => 'users#new'
     post '/users' => 'users#create'
 
+    get 'login' => 'session#new'
+    post '/login' => 'session#create'
+    get '/logout' => 'session#destroy'
   
  
 
